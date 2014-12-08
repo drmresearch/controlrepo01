413475be3d70a3f6ac6a918b28022137b3cc300e
root <root@puppetmaster.(none)>
2014-12-08 13:19:05 -0500

    initial commit controlrepo01

---
+++ site/profile/manifests/init.pp
@@ -0,0 +1,6 @@
+# Empty profile class
+# This class should do nothing and only exists for validation
+# 
+# All profiles should exist as their own subclass
+#
+class profile {}
---
+++ site/role/manifests/init.pp
@@ -0,0 +1,6 @@
+# Empty role class
+# This class should do nothing and only exists for validation
+#
+# All roles should exist as their own subclass.
+#
+class role {}
